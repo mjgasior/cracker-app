2a53c4da64a5d0580aa62310adb922c808af94e2
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-17 23:49:50 +0200

    Refactor envs

--- cracker-client/src/+components/Home.js
+++ cracker-client/src/+components/Home.js
@@ -7,6 +7,10 @@ export function Home() {
       <p>
         This is the environment build: <i>{process.env.NODE_ENV}</i>
       </p>
+      <p>{process.env.REACT_APP_API_URL}</p>
+      <p>{process.env.REACT_APP_AUTH0_ORIGIN}</p>
+      <p>{process.env.REACT_APP_AUTH0_DOMAIN}</p>
+      <p>{process.env.REACT_APP_AUTH0_CLIENT_ID}</p>
     </div>
   );
 }
