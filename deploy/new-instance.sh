5de1e35e35531a2e3430ebb9c44889639eda1e82
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-22 23:25:57 +0200

    Fixes

--- deploy/new-instance.sh
+++ deploy/new-instance.sh
@@ -1,7 +1,7 @@
 now=$(date)
 echo "$now"
 
-userdata="curl -o lightsail-compose.sh https://raw.githubusercontent.com/mjgasior/cracker-app/master/lightsail-compose.sh && chmod +x ./lightsail-compose.sh && ./lightsail-compose.sh"
+userdata="curl -o lightsail-compose.sh https://raw.githubusercontent.com/mjgasior/cracker-app/master/deploy/lightsail-compose.sh && chmod +x ./lightsail-compose.sh && ./lightsail-compose.sh"
 
 awsresult=`aws lightsail create-instances --instance-names Cracker-app --user-data "$userdata" --availability-zone eu-central-1a --blueprint-id ubuntu_16_04_2 --bundle-id nano_2_0`
 
