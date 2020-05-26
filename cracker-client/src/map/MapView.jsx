b7a38888b4b8c790b8293c2a8e09e0d5cc2702b3
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-26 21:59:24 +0200

    Resize

--- cracker-client/src/map/MapView.jsx
+++ cracker-client/src/map/MapView.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import * as parkData from "./skateparks.json";
-import "./mappy.css";
+import styled from "styled-components";
 
-export const icon = new Icon({
+const Container = styled.div`
+  width: 600px;
+  height: 300px;
+  & .leaflet-container {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const icon = new Icon({
   iconUrl: "/skateboarding.svg",
   iconSize: [25, 25],
 });
@@ -13,42 +22,42 @@ export const MapView = () => {
   const [activePark, setActivePark] = React.useState(null);
 
   return (
-    <Map center={[45.4, -75.7]} zoom={12}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-
-      {parkData.features.map((park) => (
-        <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0],
-          ]}
-          onClick={() => {
-            setActivePark(park);
-          }}
-          icon={icon}
+    <Container>
+      <Map center={[45.4, -75.7]} zoom={12}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-      ))}
 
-      {activePark && (
-        <Popup
-          position={[
-            activePark.geometry.coordinates[1],
-            activePark.geometry.coordinates[0],
-          ]}
-          onClose={() => {
-            setActivePark(null);
-          }}
-        >
-          <div>
-            <h2>{activePark.properties.NAME}</h2>
-            <p>{activePark.properties.DESCRIPTIO}</p>
-          </div>
-        </Popup>
-      )}
-    </Map>
+        {parkData.features.map((park) => (
+          <Marker
+            key={park.properties.PARK_ID}
+            position={[
+              park.geometry.coordinates[1],
+              park.geometry.coordinates[0],
+            ]}
+            onClick={() => setActivePark(park)}
+            icon={icon}
+          />
+        ))}
+
+        {activePark && (
+          <Popup
+            position={[
+              activePark.geometry.coordinates[1],
+              activePark.geometry.coordinates[0],
+            ]}
+            onClose={() => {
+              setActivePark(null);
+            }}
+          >
+            <div>
+              <h2>{activePark.properties.NAME}</h2>
+              <p>{activePark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        )}
+      </Map>
+    </Container>
   );
 };
