8170e62dd2d73a8e7756b5f7ee63e02ace625af4
Michal J. Gasior <michaljgasior@gmail.com>
2020-07-12 12:56:30 +0200

    Language switch

--- cracker-client/src/profile/ProfileView.jsx
+++ cracker-client/src/profile/ProfileView.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useCallback } from "react";
 import auth from "../+utils/Auth";
+import { Switch } from "antd";
 import { useTranslation } from "react-i18next";
 
 export const ProfileView = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const isAdmin = auth.isUserAdmin();
   const email = auth.getEmail();
-  const isEmailVerified = auth.getIsEmailVerified();
+  const verificationLabel = auth.getIsEmailVerified()
+    ? t("is_verified")
+    : t("is_not_verified");
+
+  const handleLanguageChange = useCallback(
+    (isChecked) => {
+      i18n.changeLanguage(isChecked ? "en" : "pl");
+    },
+    [i18n]
+  );
+
   return (
     <div>
       <h2>{t("profile")}</h2>
+      <p>
+        <Switch
+          checkedChildren="EN"
+          unCheckedChildren="PL"
+          onChange={handleLanguageChange}
+        />
+      </p>
       <p>
         {t("current_build")}: <i>{process.env.NODE_ENV}</i>
       </p>
@@ -23,9 +41,7 @@ export const ProfileView = () => {
       <p>
         {t("profile_data", {
           email,
-          verification: isEmailVerified
-            ? t("is_verified")
-            : t("is_not_verified"),
+          verification: verificationLabel,
         })}
       </p>
     </div>
