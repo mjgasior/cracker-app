5d5d703d496caa8bd9229d4b3cb96bb113ca4b7e
Michal J. Gasior <michaljgasior@gmail.com>
2020-07-24 19:22:40 +0200

    List

--- cracker-client/src/markersList/MarkersListView.jsx
+++ cracker-client/src/markersList/MarkersListView.jsx
@@ -1,35 +1,34 @@
 import React from "react";
-import { List, Avatar } from "antd";
+import { List, Avatar, Skeleton } from "antd";
+import { useMarkers } from "./+hooks/useMarkers";
 
-const data = [
-  {
-    title: "Ant Design Title 1",
-  },
-  {
-    title: "Ant Design Title 2",
-  },
-  {
-    title: "Ant Design Title 3",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-];
-
-export const MarkersListView = () => (
-  <List
-    itemLayout="horizontal"
-    dataSource={data}
-    renderItem={(item) => (
-      <List.Item>
-        <List.Item.Meta
-          avatar={
-            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-          }
-          title={<a href="https://ant.design">{item.title}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />
-);
+export const MarkersListView = () => {
+  const { data } = useMarkers();
+  if (data) {
+    return (
+      <List
+        itemLayout="horizontal"
+        dataSource={data.markers}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={
+                <Avatar
+                  style={{
+                    color: "#bbb",
+                    backgroundColor: "#ffd42a",
+                  }}
+                >
+                  {item.english.name[0].toUpperCase()}
+                </Avatar>
+              }
+              title={<a href="https://ant.design">{item.english.name}</a>}
+              description={item.english.description}
+            />
+          </List.Item>
+        )}
+      />
+    );
+  }
+  return <Skeleton avatar paragraph={{ rows: 4 }} />;
+};
