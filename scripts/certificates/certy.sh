3360919c5b8c70a71a83ca1e1dedcf28c025d118
Michal J. Gasior <michaljgasior@gmail.com>
2020-09-25 22:22:50 +0200

    Certy branch

--- scripts/certificates/certy.sh
+++ scripts/certificates/certy.sh
@@ -1,3 +1,6 @@
+# curl -o /srv/docker/certy.sh https://raw.githubusercontent.com/mjgasior/cracker-app/master/scripts/certificates/certy.sh
+# curl -o /srv/docker/certy.sh  https://raw.githubusercontent.com/mjgasior/cracker-app/feature/%2369-ssl-cert/scripts/certificates/certy.sh
+
 docker run -it --rm --name certbot \
     -v "/data/certbot/conf:/etc/letsencrypt" \
     -v "/data/certbot/www:/var/www/certbot" \
