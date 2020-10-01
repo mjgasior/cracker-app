603f8496ed2963a04edbe1954d78d0cec5cc9de8
Michal J. Gasior <michaljgasior@gmail.com>
2020-10-01 22:53:28 +0200

    Both methods are online

--- cracker-client/src/markers/description/UploadImage.jsx
+++ cracker-client/src/markers/description/UploadImage.jsx
@@ -2,6 +2,10 @@ import React, { useCallback } from "react";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 import { useDropzone } from "react-dropzone";
+import { Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+
+const { Dragger } = Upload;
 
 const SINGLE_UPLOAD_MUTATION = gql`
   mutation singleUpload($file: Upload!) {
@@ -14,6 +18,15 @@ export const UploadImage = () => {
 
   const onDrop = useCallback(
     ([file]) => {
+      console.log(file);
+      uploadFile({ variables: { file } });
+    },
+    [uploadFile]
+  );
+
+  const onDrop2 = useCallback(
+    ({ file }) => {
+      console.log(file);
       uploadFile({ variables: { file } });
     },
     [uploadFile]
@@ -22,13 +35,27 @@ export const UploadImage = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
-    </div>
+    <>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+      <Dragger accept={"image/*"} multiple={false} customRequest={onDrop2}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibit from uploading
+          company data or other band files
+        </p>
+      </Dragger>
+    </>
   );
 };
