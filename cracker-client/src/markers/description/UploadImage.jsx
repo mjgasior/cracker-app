d1543ead0f7c794c94f67daa2e95cd2437bbab03
Michal J. Gasior <michaljgasior@gmail.com>
2020-10-01 22:59:23 +0200

    Refactor

--- cracker-client/src/markers/description/UploadImage.jsx
+++ cracker-client/src/markers/description/UploadImage.jsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from "react";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
-import { useDropzone } from "react-dropzone";
 import { Upload } from "antd";
 import { InboxOutlined } from "@ant-design/icons";
 
@@ -17,45 +16,22 @@ export const UploadImage = () => {
   const [uploadFile] = useMutation(SINGLE_UPLOAD_MUTATION);
 
   const onDrop = useCallback(
-    ([file]) => {
-      console.log(file);
-      uploadFile({ variables: { file } });
-    },
+    ({ file }) => uploadFile({ variables: { file } }),
     [uploadFile]
   );
-
-  const onDrop2 = useCallback(
-    ({ file }) => {
-      console.log(file);
-      uploadFile({ variables: { file } });
-    },
-    [uploadFile]
-  );
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here...</p>
-        ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        )}
-      </div>
-      <Dragger accept={"image/*"} multiple={false} customRequest={onDrop2}>
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">
-          Click or drag file to this area to upload
-        </p>
-        <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading
-          company data or other band files
-        </p>
-      </Dragger>
-    </>
+    <Dragger accept={"image/*"} multiple={false} customRequest={onDrop}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading
+        company data or other band files
+      </p>
+    </Dragger>
   );
 };
