d22cc17d85a42f96764c142e2df9fb869cf104ab
Michal J. Gasior <michaljgasior@gmail.com>
2020-10-15 23:00:40 +0200

    Cleanup

--- cracker-server/src/+services/imageService.js
+++ cracker-server/src/+services/imageService.js
@@ -1,18 +1,8 @@
-import express from "express";
 import sharp from "sharp";
 import path from "path";
 
 export const resizeImage = async (req, res, next) => {
-  console.log(__dirname);
-
   const filepath = path.join(__dirname, `../../images${req.path}`);
-  console.log(filepath);
-
-  sharp(filepath)
-    .resize(parseInt(req.query.w), parseInt(req.query.h))
-    .toFile("images/output.webp", (err, info) => {
-      console.log("DONE");
-    });
 
   const streamImage = await sharp(filepath)
     .resize(parseInt(req.query.w), parseInt(req.query.h))
@@ -22,5 +12,4 @@ export const resizeImage = async (req, res, next) => {
   res.write(streamImage);
   res.end();
   next();
-  // express.static("images")(req, res, next);
 };
