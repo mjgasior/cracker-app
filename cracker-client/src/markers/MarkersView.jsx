ac9086e11999b4a1d68cc32db2d3f34fa5245a0d
Michal J. Gasior <michaljgasior@gmail.com>
2020-10-21 22:16:47 +0200

    Get back

--- cracker-client/src/markers/MarkersView.jsx
+++ cracker-client/src/markers/MarkersView.jsx
@@ -1,24 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Row, Col } from "antd";
 import { MapView } from "./map/MapView";
 import { Description } from "./description/Description";
 import { useUser } from "../+hooks/useUser";
 import { useMarkers } from "./+hooks/useMarkers";
-import { useMarkerRouteHandler } from "./+hooks/useMarkerRouteHandler";
+import { useHistory, useRouteMatch } from "react-router-dom";
 
 export const MarkersView = () => {
   const { data } = useMarkers();
   const { isAdmin } = useUser();
   const [currentMarker, setCurrentMarker] = useState(null);
 
-  const routeHandler = useMarkerRouteHandler();
+  const history = useHistory();
+  const match = useRouteMatch("/markers/:markerid");
 
   useEffect(() => {
     if (match && data && currentMarker === null) {
       const marker = data.markers.find((x) => x._id === match.params.markerid);
       setCurrentMarker(marker);
     }
-  }, [match, data, currentMarker]);
+  }, [match, data, currentMarker, setCurrentMarker]);
+
+  const routeHandler = useCallback(
+    (selectedMarkerId) => {
+      if (match && match.isExact) {
+        history.replace(selectedMarkerId);
+      } else {
+        history.push(`/markers/${selectedMarkerId}`);
+      }
+    },
+    [history, match]
+  );
 
   return (
     <Row>
